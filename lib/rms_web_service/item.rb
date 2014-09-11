f20a073a5c6c8df1af5d15e0073c2a5bb243483a
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-09-11 09:00:24 +0900

    Move RmsWebSErvice::Item.delete.

--- lib/rms_web_service/item.rb
+++ lib/rms_web_service/item.rb
@@ -58,6 +58,21 @@ module RmsWebService
         request = connection("item/update").post {|req| req.body = xml_object.to_xml}
         return Update.new(request.body)
       end
+
+      def delete(args)
+        xml_object = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+          xml.request do
+            xml.itemDeleteRequest do
+              xml.item do
+                args.each {|key, value| eval("xml.#{key.to_s.camelize(:lower)} '#{value}'")}
+              end
+            end
+          end
+        end
+
+        request = connection("item/delete").post {|req| req.body = xml_object.to_xml}
+        return Delete.new(request.body)
+      end
     end
   end
 end
--- lib/rms_web_service/item/delete.rb
+++ lib/rms_web_service/item/delete.rb
@@ -1,20 +1,5 @@
 module RmsWebService
   class Item
-    def self.delete(args)
-      xml_object = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
-        xml.request do
-          xml.itemDeleteRequest do
-            xml.item do
-              args.each {|key, value| eval("xml.#{key.to_s.camelize(:lower)} '#{value}'")}
-            end
-          end
-        end
-      end
-
-      request = connection("item/delete").post {|req| req.body = xml_object.to_xml}
-      return Delete.new(request.body)
-    end
-
     class Delete
       include Response
       def initialize(xml)
