7c138f6ce037218eddc869d4cbdbf73526bf2e71
kamiya54 <kamiya54@gmail.com>
2014-09-17 22:54:02 +0900

    Add 'field' and 'field_name' attributes to Response::Item.errors.

--- lib/rms_web_service/response/item/error.rb
+++ lib/rms_web_service/response/item/error.rb
@@ -1,11 +1,17 @@
+#encoding: utf-8
 module RmsWebService
   module Response
     module Item
       class Error
+        require 'rms_web_service/response/item/error/const'
         def self.parse(xml)
           raise ArgumentError, "Argument must be a String class. but argument is #{xml.class}." unless xml.class == String
           hash = Hash.new
           Nokogiri::XML.parse(xml).xpath("//errorMessage").children.each {|att| hash.store(att.name.underscore, att.content)}
+          if hash['field_id'] && ::FIELDID_LIST["#{hash['field_id']}"].present?
+            hash.store("field", ::FIELDID_LIST["#{hash['field_id']}"][0].underscore)
+            hash.store("field_name", ::FIELDID_LIST["#{hash['field_id']}"][1].underscore)
+          end
           return hash
         end
       end
--- spec/rms_web_service/response/item/error_spec.rb
+++ spec/rms_web_service/response/item/error_spec.rb
@@ -1,14 +1,15 @@
+#encoding: utf-8
 require 'spec_helper'
 require 'rms_web_service'
 
 describe RWS::Response::Item::Error do
+  let(:res) {{"error_id"=>"errorId", "field_id"=>"503", "msg"=>"error message", "list_index"=>"1", "list_index2"=>"2", "field"=>"item_price", "field_name"=>"販売価格"}}
   subject {described_class.parse(fixture('update.xml').read)}
-  it {is_expected.to be_a Hash}
+  it("should return formatted error") {is_expected.to eq res}
 
-  describe "attributes" do
-    it {is_expected.to include('error_id')}
-    it {is_expected.to include('field_id')}
-    it {is_expected.to include('msg')}
+  describe 'optional attributes' do
+    it {is_expected.to include("field")}
+    it {is_expected.to include("field_name")}
   end
 
   describe 'invalid argument' do
